168892515c1a421f65c8a10444b69ffe370fdd7b
artembels <belsartem@gmail.com>
2026-06-14 06:56:22 +0200

    Исправлено: все действия теперь в одну строку для удобства чтения

--- src/test/java/RegistrationFormTests.java
+++ src/test/java/RegistrationFormTests.java
@@ -26,69 +26,29 @@ public class RegistrationFormTests extends TestBase {
         $(".react-datepicker__year-select").selectOption("1995");
         $(".react-datepicker__day--001:not(.react-datepicker__day--outside-month)").click();
         $("#subjectsInput").setValue("computer").pressEnter();
-        $("#hobbiesWrapper").$$("label")
-                .findBy(text("Sports"))
-                .click();
-        $("#hobbiesWrapper").$$("label")
-                .findBy(text("Music"))
-                .click();
-        $("#uploadPicture")
-                .uploadFromClasspath("images/scottish_cat.png");
-        $("#uploadPicture")
-                .shouldHave(value("cat.png"));
+        $("#hobbiesWrapper").$$("label").findBy(text("Sports")).click();
+        $("#hobbiesWrapper").$$("label").findBy(text("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("images/scottish_cat.png");
+        $("#uploadPicture").shouldHave(value("cat.png"));
         $("#currentAddress").setValue("1st street");
         $("#state").click();
-        $$("[id^=react-select-3-option]")
-                .findBy(text("Uttar Pradesh"))
-                .click();
+        $$("[id^=react-select-3-option]").findBy(text("Uttar Pradesh")).click();
         $("[id=city]").click();
-        $$("[id^=react-select-4-option]")
-                .findBy(text("Merrut"))
-                .click();
+        $$("[id^=react-select-4-option]").findBy(text("Merrut")).click();
 
         $("#submit").click();
 
         $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive")
-                .$(byText("Student Name"))
-                .closest("tr")
-                .shouldHave(text("Test Name Test Last Name"));
-        $(".table-responsive")
-                .$(byText("Student Email"))
-                .closest("tr")
-                .shouldHave(text("test@test.com"));
-        $(".table-responsive")
-                .$(byText("Gender"))
-                .closest("tr")
-                .shouldHave(text("Male"));
-        $(".table-responsive")
-                .$(byText("Mobile"))
-                .closest("tr")
-                .shouldHave(text("0123456789"));
-        $(".table-responsive")
-                .$(byText("Date of Birth"))
-                .closest("tr")
-                .shouldHave(text("01 October,1995"));
-        $(".table-responsive")
-                .$(byText("Subjects"))
-                .closest("tr")
-                .shouldHave(text("Computer Science"));
-        $(".table-responsive")
-                .$(byText("Hobbies"))
-                .closest("tr")
-                .shouldHave(text("Sports, Music"));
-        $(".table-responsive")
-                .$(byText("Picture"))
-                .closest("tr")
-                .shouldHave(text("scottish_cat.png"));
-        $(".table-responsive")
-                .$(byText("Address"))
-                .closest("tr")
-                .shouldHave(text("1st street"));
-        $(".table-responsive")
-                .$(byText("State and City"))
-                .closest("tr")
-                .shouldHave(text("Uttar Pradesh Merrut"));
+        $(".table-responsive").$(byText("Student Name")).closest("tr").shouldHave(text("Test Name Test Last Name"));
+        $(".table-responsive").$(byText("Student Email")).closest("tr").shouldHave(text("test@test.com"));
+        $(".table-responsive").$(byText("Gender")).closest("tr").shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile")).closest("tr").shouldHave(text("0123456789"));
+        $(".table-responsive").$(byText("Date of Birth")).closest("tr").shouldHave(text("01 October,1995"));
+        $(".table-responsive").$(byText("Subjects")).closest("tr").shouldHave(text("Computer Science"));
+        $(".table-responsive").$(byText("Hobbies")).closest("tr").shouldHave(text("Sports, Music"));
+        $(".table-responsive").$(byText("Picture")).closest("tr").shouldHave(text("scottish_cat.png"));
+        $(".table-responsive").$(byText("Address")).closest("tr").shouldHave(text("1st street"));
+        $(".table-responsive").$(byText("State and City")).closest("tr").shouldHave(text("Uttar Pradesh Merrut"));
     }
 
     @Test
@@ -108,18 +68,9 @@ public class RegistrationFormTests extends TestBase {
         $("#submit").click();
 
         $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive")
-                .$(byText("Student Name"))
-                .closest("tr")
-                .shouldHave(text("Test Name Test Last Name"));
-        $(".table-responsive")
-                .$(byText("Gender"))
-                .closest("tr")
-                .shouldHave(text("Male"));
-        $(".table-responsive")
-                .$(byText("Mobile"))
-                .closest("tr")
-                .shouldHave(text("0123456789"));
+        $(".table-responsive").$(byText("Student Name")).closest("tr").shouldHave(text("Test Name Test Last Name"));
+        $(".table-responsive").$(byText("Gender")).closest("tr").shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile")).closest("tr").shouldHave(text("0123456789"));
     }
 
     @Test
